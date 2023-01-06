796c06b6fb92a0d12fbf425fad2a7340a1e1f81a
SoftPoachedEggs <115499632+SoftPoachedEggs@users.noreply.github.com>
2023-01-06 12:51:20 -0500

    fixed bug in color logic... 9am was not updating.

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,19 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+let now = dayjs();
+console.log(now.format());
+
+const timeBlock = document.querySelector("hour");
+
+console.log(timeBlock);
+
 $(function () {
+//updateTimeandDate();
+//displayTasks();
+  });
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -14,10 +26,35 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+const displayColorUpdate = () => {
+  //format current time to hour and save as current time
+  var currentTime = parseInt(now.format("H"));
+  console.log("current time: " + currentTime)
+  for (i = 9; i <= 17; i++) {
+    if (i === currentTime){
+      $("#" + i).css("background-color", "#8ccceb")
+      console.log("current")
+    } else if (i > currentTime){
+      $("#" + i).css("background-color", "#a2f2a6")
+      console.log("future")
+    } else {
+      $("#" + i).css("background-color", "#abbfc9")
+      console.log("past")
+    } 
+    console.log("jquery target: " + "#" + i);
+  } 
+
+  
+  console.log()
+}
+
+displayColorUpdate();
+
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
